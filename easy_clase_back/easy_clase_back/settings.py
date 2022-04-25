568568cec7a1461caf769df84af4cfec883a02cb
perezpefaur <perezpefaur@uc.cl>
2022-04-25 16:14:43 -0400

    Filtros de busqueda implementados

--- easy_clase_back/easy_clase_back/settings.py
+++ easy_clase_back/easy_clase_back/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
     'rest_framework',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'django_filters'
 ]
 
 REST_FRAMEWORK = {
