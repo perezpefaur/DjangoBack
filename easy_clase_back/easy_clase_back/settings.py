abd85206a97a2e967b608d03a571821c2553c207
Martin Ruiz-Tagle <martinrtc@uc.cl>
2022-04-25 16:57:55 -0400

    cors fix

--- easy_clase_back/easy_clase_back/settings.py
+++ easy_clase_back/easy_clase_back/settings.py
@@ -93,6 +93,9 @@ DATABASES = {
     }
 }
 
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
