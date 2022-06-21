b8f350e787960f722f4daf18d28e590d6ebf818e
Rodrigo Heiremans <rheiremans@uc.cl>
2022-06-20 21:36:41 -0400

    ✅  Agregue un par de tests a comments

--- easy_clase_back/api/tests/test_comments.py
+++ easy_clase_back/api/tests/test_comments.py
@@ -1,12 +1,9 @@
-import json
 from rest_framework.test import APITestCase
 from django.contrib.auth import get_user_model
-from django.urls import reverse
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
-from api.serializers import RegisterSerializer, UserSerializer
-from api.models import UserProfile, Module, Reservation
-from unittest import skip
+from api.models import Module, Reservation
+from api.models import Comment as CommentModel
 
 
 class Comments(APITestCase):
@@ -226,3 +223,146 @@ class Comments(APITestCase):
         self.assertEqual(response.data[0]['body'], 'Excelente profe')
         self.assertEqual(response2.status_code, status.HTTP_200_OK)
         self.assertEqual(response2.data[0]['rating'], 4.3)
+
+    def test_update_comment(self):
+
+        self.student = get_user_model().objects.create_user(
+            mail="student@uc.cl",
+            password="pass1234test",
+            first_name="student",
+            last_name="student",
+            phone="66783359",
+            is_student=True)
+
+        self.teacher = get_user_model().objects.create_user(
+            mail="teacher@uc.cl",
+            password="pass1234test",
+            first_name="teacher",
+            last_name="teacher",
+            phone="66783309",
+            is_teacher=True)
+
+        self.token = RefreshToken.for_user(user=self.student).access_token
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + str(self.token))
+
+        self.module = Module.objects.create(teacher=self.teacher, start_time="13:00:00",
+                                            end_time="14:00:00", date="2023-05-05")
+        self.reservation = Reservation.objects.create(student=self.student, module=self.module, teacher_done=True, student_done=True)
+        
+        self.comment = CommentModel.objects.create(reservation=self.reservation, body="Wenisimo", rating=4.3)
+
+        response = self.client.patch(
+            f'/api/comment/?id={self.comment.id}', {"rating": 4.2})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["rating"], 4.2)
+
+    def test_update_alien_comment(self):
+
+        self.student = get_user_model().objects.create_user(
+            mail="student@uc.cl",
+            password="pass1234test",
+            first_name="student",
+            last_name="student",
+            phone="66783359",
+            is_student=True)
+
+        self.alien = get_user_model().objects.create_user(
+            mail="alien@uc.cl",
+            password="pass1234test",
+            first_name="alien",
+            last_name="alien",
+            phone="66783322",
+            is_student=True)
+
+        self.teacher = get_user_model().objects.create_user(
+            mail="teacher@uc.cl",
+            password="pass1234test",
+            first_name="teacher",
+            last_name="teacher",
+            phone="66783309",
+            is_teacher=True)
+
+        self.token = RefreshToken.for_user(user=self.student).access_token
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + str(self.token))
+
+        self.module = Module.objects.create(teacher=self.teacher, start_time="13:00:00",
+                                            end_time="14:00:00", date="2023-05-05")
+        self.reservation = Reservation.objects.create(student=self.alien, module=self.module, teacher_done=True, student_done=True)
+        
+        self.comment = CommentModel.objects.create(reservation=self.reservation, body="Wenisimo", rating=4.3)
+
+        response = self.client.patch(
+            f'/api/comment/?id={self.comment.id}', {"rating": 4.3})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.data["detail"], "You are not the owner of this comment")
+
+    def test_delete_comment(self):
+
+        self.student = get_user_model().objects.create_user(
+            mail="student@uc.cl",
+            password="pass1234test",
+            first_name="student",
+            last_name="student",
+            phone="66783359",
+            is_student=True)
+
+        self.teacher = get_user_model().objects.create_user(
+            mail="teacher@uc.cl",
+            password="pass1234test",
+            first_name="teacher",
+            last_name="teacher",
+            phone="66783309",
+            is_teacher=True)
+
+        self.token = RefreshToken.for_user(user=self.student).access_token
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + str(self.token))
+
+        self.module = Module.objects.create(teacher=self.teacher, start_time="13:00:00",
+                                            end_time="14:00:00", date="2023-05-05")
+        self.reservation = Reservation.objects.create(student=self.student, module=self.module, teacher_done=True, student_done=True)
+        
+        self.comment = CommentModel.objects.create(reservation=self.reservation, body="Wenisimo", rating=4.3)
+
+        response = self.client.delete(
+            f'/api/comment/?id={self.comment.id}')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_delete_alien_comment(self):
+
+        self.student = get_user_model().objects.create_user(
+            mail="student@uc.cl",
+            password="pass1234test",
+            first_name="student",
+            last_name="student",
+            phone="66783359",
+            is_student=True)
+
+        self.alien = get_user_model().objects.create_user(
+            mail="alien@uc.cl",
+            password="pass1234test",
+            first_name="alien",
+            last_name="alien",
+            phone="66783322",
+            is_student=True)
+
+        self.teacher = get_user_model().objects.create_user(
+            mail="teacher@uc.cl",
+            password="pass1234test",
+            first_name="teacher",
+            last_name="teacher",
+            phone="66783309",
+            is_teacher=True)
+
+        self.token = RefreshToken.for_user(user=self.student).access_token
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + str(self.token))
+
+        self.module = Module.objects.create(teacher=self.teacher, start_time="13:00:00",
+                                            end_time="14:00:00", date="2023-05-05")
+        self.reservation = Reservation.objects.create(student=self.alien, module=self.module, teacher_done=True, student_done=True)
+        
+        self.comment = CommentModel.objects.create(reservation=self.reservation, body="Wenisimo", rating=4.3)
+
+        response = self.client.delete(
+            f'/api/comment/?id={self.comment.id}', {"rating": 4.3})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.data["detail"], "You are not the owner of this comment")
