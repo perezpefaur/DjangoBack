1cc243b82ae54094ba4c093c25fe48709e49e78e
juanpacl111 <juan.pablo.contreras.g@gmail.com>
2022-06-24 00:23:29 -0400

    init

--- easy_clase_back/easy_clase_back/settings.py
+++ easy_clase_back/easy_clase_back/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-79aeei4va+e!=117+7cqg%yjk-b^15@no4n3&*66_4#zzb@ubp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = ((os.environ.get('DEBUG_VALUE')) == "TRUE")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['easyclassback.herokuapp.com']
 
 
 # Application definition
@@ -139,8 +140,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static_root/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -150,3 +151,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'api.UserProfile'
+
+django_heroku.settings(locals())
